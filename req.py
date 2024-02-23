65fb25c4f900a277a599f5cf1de48696b1f18403
galerra <artem-newalex@mail.ru>
2024-02-23 19:26:01 +0300

    Второй коммит

--- req.py
+++
@@ -1,4 +0,0 @@
-import requests
-from conversions import linkProcessing
-response = requests.get(linkProcessing("+79614951406", "aaa"))
-print(response.headers)
--- systemSettings.py
+++
@@ -1,6 +0,0 @@
-from platform import system
-from tkinter import *
-
-def getScreenSize():
-    win = Tk()
-    return {"width": win.winfo_screenwidth(), "height":win.winfo_screenheight()}
