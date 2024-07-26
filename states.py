6bb240e0e6b399754664461ff7cc872de99d3dd2
galerra <91934759+galerra@users.noreply.github.com>
2024-07-26 21:46:35 +0300

    Delete states.py

--- states.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-class Status(StatesGroup):
-    waitingUserInfo = State() # Задаем состояние
-    waitingMessengerSelection = State()
-    waitingAdminInfo = State()
-    waitingRecordInfo = State()
-    waitingDeletionWorker = State()
-    waitingIdDeletableRecord = State()
-    waitingCommandPassword = State()
