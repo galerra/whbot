4ac99fb205ec3e66ccf66d2b8f76d962bc92751a
galerra <91934759+galerra@users.noreply.github.com>
2024-07-26 23:37:30 +0300

    Update whatsappActions.py

--- whatsappActions.py
+++ whatsappActions.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.chrome import ChromeDriverManager
 from conversions import linkProcessing
@@ -24,7 +24,7 @@ def chromeSetup():
     options.add_argument('--profile-directory=Profile 1')
     options.add_argument('--profiling-flush=n')
     options.add_argument('--enable-aggressive-domstorage-flushing')
-    #driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(service = service, options=options)
     # driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
     return driver
 
